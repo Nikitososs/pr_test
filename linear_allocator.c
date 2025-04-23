8117c9ed2a5e96d8881abd77199c3ff34ba26c15
Nikitososs <nsmirnow.n@gmail.com>
2025-04-23 20:58:58 +0600

    feat: added basic linear allocator realization

---
+++ linear_allocator.c
@@ -0,0 +1,35 @@
+#include "linear_allocator.h"
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+Linear_allocator linear_init(size_t volume) {
+  void *data = malloc(sizeof(uint8_t) * volume);
+  Linear_allocator linear_allocator = {
+      .volume = volume,
+      .size = 0,
+      .data = data,
+  };
+  return linear_allocator;
+}
+
+void *linear_alloc(Linear_allocator *linear_allocator, size_t size) {
+  if (linear_allocator->size + size > linear_allocator->volume) {
+    return NULL;
+  }
+  int8_t *data = &linear_allocator->data[linear_allocator->size];
+  linear_allocator->size += size;
+  return data;
+}
+
+void linear_reset(Linear_allocator *linear_allocator) {
+  linear_allocator->size = 0;
+}
+
+void linear_free(Linear_allocator *linear_allocator) {
+  linear_allocator->volume = 0;
+  linear_allocator->size = 0;
+  free(linear_allocator->data);
+}
