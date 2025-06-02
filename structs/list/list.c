9e4aa7a7e3dcac71ebcca653d9a75967e006b5fc
Nikitososs <nsmirnow.n@gmail.com>
2025-06-03 02:33:50 +0600

    fix: fix mem leaks

--- structs/list/list.c
+++ structs/list/list.c
@@ -3,87 +3,106 @@
 #include <stdlib.h>
 
 struct list *create_list(int value) {
-  struct list *ls = (struct list *)malloc(sizeof(struct list));
-  ls->val = value;
-  return ls;
+    struct list *ls = (struct list *)malloc(sizeof(struct list));
+    if (!ls) return NULL;
+    ls->val = value;
+    ls->next = NULL;
+    return ls;
 }
 
 int length(struct list *ls) {
-  if (ls == NULL)
-    return 0;
-  int i = 1;
-  while (ls->next != NULL) {
-    ls = ls->next;
-    i += 1;
-  }
-  return i;
+    int i = 0;
+    while (ls != NULL) {
+        ls = ls->next;
+        i++;
+    }
+    return i;
 }
 
 int add_to_end(struct list *ls, int value) {
-  while (ls->next != NULL)
-    ls = ls->next;
-  struct list *new = (struct list *)malloc(sizeof(struct list));
-  new->val = value;
-  ls->next = new;
-  return 0;
+    if (!ls) return 1;
+    while (ls->next != NULL)
+        ls = ls->next;
+
+    struct list *new_node = (struct list *)malloc(sizeof(struct list));
+    if (!new_node) return 1;
+
+    new_node->val = value;
+    new_node->next = NULL;
+    ls->next = new_node;
+    return 0;
 }
 
-int add_in_start(struct list *ls, int value) {
-  int next = ls->val;
-  ls->val = value;
-  while (ls->next != NULL) {
-    ls = ls->next;
-    next = ls->val;
-    ls->val = next;
-  }
-  ls->next = (struct list *)malloc(sizeof(struct list));
-  ls->next->val = next;
-  return 0;
+int add_in_start(struct list **ls, int value) {
+    struct list *new_node = (struct list *)malloc(sizeof(struct list));
+    if (!new_node) return 1;
+
+    new_node->val = value;
+    new_node->next = *ls;
+    *ls = new_node;
+    return 0;
 }
 
 int add_after_index(struct list *ls, int index, int value) {
-  if (index < 0)
+    int i = 0;
+    while (ls != NULL && i < index) {
+        ls = ls->next;
+        i++;
+    }
+
+    if (!ls) return 1;
+
+    struct list *new_node = (struct list *)malloc(sizeof(struct list));
+    if (!new_node) return 1;
+
+    new_node->val = value;
+    new_node->next = ls->next;
+    ls->next = new_node;
     return 0;
-  int i = 0;
-  while (ls->next != NULL && i < index) {
-    ls = ls->next;
-    i += 1;
-  }
-  struct list *next = ls->next;
-  ls->next = (struct list *)malloc(sizeof(struct list));
-  ls->next->val = value;
-  ls->next->next = next;
-  return 0;
 }
 
-int find_by_index(struct list *ls, int index) {
-  if (index < 0)
-    return 0;
-  int i = 0;
-  while (ls->next != NULL && i < index) {
-    ls = ls->next;
-    i += 1;
-  }
-  return ls->val;
+int find_by_index(struct list *head, int index) {
+    if (head == NULL || index < 0)
+        return -1;
+
+    struct list *cur = head;
+    for (int i = 0; i < index && cur != NULL; ++i) {
+        cur = cur->next;
+    }
+
+    if (cur == NULL)
+        return -1;
+
+    return cur->val;
 }
-int remove_by_index(struct list *ls, int index) {
-  if (index < 0)
+
+int remove_by_index(struct list **ls, int index) {
+    if (!*ls || index < 0) return 1;
+
+    struct list *temp = *ls;
+
+    if (index == 0) {
+        *ls = temp->next;
+        free(temp);
+        return 0;
+    }
+
+    for (int i = 0; temp != NULL && i < index - 1; i++) {
+        temp = temp->next;
+    }
+
+    if (!temp || !temp->next) return 1;
+
+    struct list *to_delete = temp->next;
+    temp->next = to_delete->next;
+    free(to_delete);
     return 0;
-  int i = 0;
-  while (ls->next != NULL && i < index) {
-    ls = ls->next;
-    i += 1;
-  }
-  if (i == 0)
-    ;
-  *ls = *ls->next;
-  return 1;
 }
+
 void destroy(struct list *ls) {
-  while (ls->next != NULL) {
-    struct list *prev = ls;
-    ls = ls->next;
-    free(prev);
-  }
-  free(ls);
+    while (ls != NULL) {
+        struct list *next = ls->next;
+        free(ls);
+        ls = next;
+    }
 }
